808f89c231aa3b7a417cf27dec3d87fb7d598190
Davi Campos <arlocm@gmail.com>
2012-11-07 10:47:32 -0200

    Adding debug line

--- files/install_package.sh
+++ files/install_package.sh
@@ -7,4 +7,6 @@ package_name=$2
 #venv_path=$HOME/.virtualenvs
 venv_path=$WORKON_HOME
 
+echo "pip install --environment=$venv_path/$venv_name $package_name" > /tmp/wrapper_debug
+
 pip install --environment=$venv_path/$venv_name $package_name
