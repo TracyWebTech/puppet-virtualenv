87b3c5e01b794e89e93aa659852d6a568f7cbc65
Davi Campos <arlocm@gmail.com>
2012-11-07 14:21:25 -0200

    Fixing virtualenv::create to use correctly project

--- files/mkvirtualenv.sh
+++ files/mkvirtualenv.sh
@@ -3,7 +3,12 @@ source /etc/bash_completion.d/virtualenvwrapper;
 
 venv_name=$1
 venv_project=$2
+opts=""
+
+if [ ${venv_project} != "" ]; then
+    opts="-a ${venv_project}"
+fi
 
 if [ ! -d "$WORKON_HOME/$venv_name" ]; then
-    mkvirtualenv $1 $2
+    mkvirtualenv ${opts} $1
 fi
