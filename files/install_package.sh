f8d6695a6c511ed77e97836fe3be2618cfc1b854
Davi Campos <arlocm@gmail.com>
2012-11-07 10:51:34 -0200

    Removing bug line

--- files/install_package.sh
+++ files/install_package.sh
@@ -7,6 +7,4 @@ package_name=$2
 #venv_path=$HOME/.virtualenvs
 venv_path=$WORKON_HOME
 
-echo "pip install --environment=$venv_path/$venv_name $package_name" > /tmp/wrapper_debug
-
 pip install --environment=$venv_path/$venv_name $package_name
