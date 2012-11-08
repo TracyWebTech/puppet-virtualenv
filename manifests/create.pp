57f159486eae3477bd46e3871378e90235ac9465
Davi Campos <arlocm@gmail.com>
2012-11-08 10:15:25 -0200

    Adding command to update distribute becausa ubuntu uses a very old version

--- manifests/create.pp
+++ manifests/create.pp
@@ -12,12 +12,21 @@ define virtualenv::create(
     unless  => "test -d ${venv}/bin",
   }
 
+  exec { "update distribute ${venv} ${requirements}":
+    command     => "pip install -U distribute",
+    user        => $user,
+    path        => ["${venv}/bin"],
+    subscribe   => Exec["create_virtualenv ${venv}"],
+    refreshonly => true,
+  }
+
   if $requirements {
     exec { "install_requirements ${venv} ${requirements}":
-      command     => "pip install --environment=${venv} -r ${requirements}",
+      command     => "pip install -r ${requirements}",
       user        => $user,
-      path        => ['/usr/bin'],
+      path        => ["${venv}/bin"],
       subscribe   => Exec["create_virtualenv ${venv}"],
+      require     => Exec["update distribute ${venv} ${requirements}"],
       refreshonly => true,
     }
   }
