7417b4c735eb5d1dbb41c398d9f5c43f0a65f020
Davi Campos <arlocm@gmail.com>
2012-11-08 15:25:52 -0200

    Moving virtualenv::virtualenv to virtualenv.pp and fix a bug in commando to install package

---
+++ manifests/virtualenv.pp
@@ -0,0 +1,62 @@
+define virtualenv::virtualenv(
+  $venv = $title,
+  $ensure = present,
+  $user = undef,
+  $requirements = undef,
+  $packages = undef
+) {
+
+  if $ensure == absent {
+    file { $venv:
+      ensure => absent,
+      force  => true,
+    }
+  } elsif $ensure == present {
+    if !$user {
+      fail('user is required to ensure present')
+    }
+    exec { "create_virtualenv ${venv}":
+      command => "virtualenv ${venv}",
+      user    => $user,
+      require => Package['python-virtualenv'],
+      path    => ['/usr/bin'],
+      creates => "${venv}/bin/",
+    }
+
+    exec { "update distribute ${venv} ${requirements}":
+      command     => "pip install -U distribute",
+      user        => $user,
+      path        => ["${venv}/bin", "/usr/local/bin", "/usr/bin", "/bin"],
+      subscribe   => Exec["create_virtualenv ${venv}"],
+      refreshonly => true,
+    }
+
+    if $requirements {
+      exec { "install_requirements ${venv} ${requirements}":
+        command     => "pip install -r ${requirements}",
+        user        => $user,
+        path        => ["${venv}/bin", "/usr/local/bin", "/usr/bin", "/bin"],
+        subscribe   => Exec["create_virtualenv ${venv}"],
+        require     => Exec["update distribute ${venv} ${requirements}"],
+        refreshonly => true,
+        timeout     => 0,
+      }
+    }
+
+    if $packages {
+      exec { "install_packages ${venv} ${packages}":
+        command     => "pip install ${packages}",
+        user        => $user,
+        path        => ["${venv}/bin", "/usr/local/bin", "/usr/bin", "/bin"],
+        subscribe   => Exec["create_virtualenv ${venv}"],
+        require     => Exec["update distribute ${venv} ${requirements}"],
+        refreshonly => true,
+        timeout     => 0,
+      }
+    }
+  } else {
+    fail('invalid ensure')
+  }
+
+
+}
