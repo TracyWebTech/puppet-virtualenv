a00a6f64891171cab8de9b464ef46d3c7039d18f
Davi Campos <arlocm@gmail.com>
2012-11-09 15:59:46 -0200

    Chaning if,elsif and else with case

--- manifests/virtualenv.pp
+++ manifests/virtualenv.pp
@@ -6,57 +6,61 @@ define virtualenv::virtualenv(
   $packages = undef
 ) {
 
-  if $ensure == absent {
-    file { $venv:
-      ensure => absent,
-      force  => true,
-    }
-  } elsif $ensure == present {
-    if !$user {
-      fail('user is required to ensure present')
-    }
-    exec { "create_virtualenv ${venv}":
-      command => "virtualenv ${venv}",
-      user    => $user,
-      require => Package['python-virtualenv'],
-      path    => ['/usr/bin'],
-      creates => "${venv}/bin/",
-    }
+  case $ensure {
 
-    exec { "update distribute ${venv}":
-      command     => "pip install -U distribute",
-      user        => $user,
-      path        => ["${venv}/bin", "/usr/local/bin", "/usr/bin", "/bin"],
-      subscribe   => Exec["create_virtualenv ${venv}"],
-      refreshonly => true,
+    absent: {
+      file { $venv:
+        ensure => absent,
+        force  => true,
+      }
     }
+    present: {
+      if !$user {
+        fail('user is required to ensure present')
+      }
+      exec { "create_virtualenv ${venv}":
+        command => "virtualenv ${venv}",
+        user    => $user,
+        require => Package['python-virtualenv'],
+        path    => ['/usr/bin'],
+        creates => "${venv}/bin/",
+      }
 
-    if $requirements {
-      exec { "install_requirements ${venv} ${requirements}":
-        command     => "pip install -r ${requirements}",
+      exec { "update distribute ${venv}":
+        command     => "pip install -U distribute",
         user        => $user,
         path        => ["${venv}/bin", "/usr/local/bin", "/usr/bin", "/bin"],
         subscribe   => Exec["create_virtualenv ${venv}"],
-        require     => Exec["update distribute ${venv} ${requirements}"],
         refreshonly => true,
-        timeout     => 0,
       }
-    }
 
-    if $packages {
-      exec { "install_packages ${venv} ${packages}":
-        command     => "pip install ${packages}",
-        user        => $user,
-        path        => ["${venv}/bin", "/usr/local/bin", "/usr/bin", "/bin"],
-        subscribe   => Exec["create_virtualenv ${venv}"],
-        require     => Exec["update distribute ${venv} ${requirements}"],
-        refreshonly => true,
-        timeout     => 0,
+      if $requirements {
+        exec { "install_requirements ${venv} ${requirements}":
+          command     => "pip install -r ${requirements}",
+          user        => $user,
+          path        => ["${venv}/bin", "/usr/local/bin", "/usr/bin", "/bin"],
+          subscribe   => Exec["create_virtualenv ${venv}"],
+          require     => Exec["update distribute ${venv}"],
+          refreshonly => true,
+          timeout     => 0,
+        }
+      }
+
+      if $packages {
+        exec { "install_packages ${venv} ${packages}":
+          command     => "pip install ${packages}",
+          user        => $user,
+          path        => ["${venv}/bin", "/usr/local/bin", "/usr/bin", "/bin"],
+          subscribe   => Exec["create_virtualenv ${venv}"],
+          require     => Exec["update distribute ${venv}"],
+          refreshonly => true,
+          timeout     => 0,
+        }
       }
     }
-  } else {
-    fail('invalid ensure')
+    default: {
+      fail('invalid ensure')
+    }
   }
-
 
 }
